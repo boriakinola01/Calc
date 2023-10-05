cbaae3ff994d09a4d2f2269c54db6f74b61b5db7
Bori <boriakinola01@gmail.com>
2023-10-05 10:17:07 +0100

    Initial commit. Adding changes to git

---
+++ Calc/CalcModel.swift
@@ -0,0 +1,8 @@
+//
+//  CalcModel.swift
+//  Calc
+//
+//  Created by Bori Akinola on 23/09/2023.
+//
+
+import Foundation
--- Calc/CalcVC.swift
+++ Calc/CalcVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalcVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
